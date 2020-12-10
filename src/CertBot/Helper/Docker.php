b1fbf760662447fcec076e47b25096a5658adc39
Anton Boutkam <anton@nui-boutkam.nl>
2020-12-10 03:41:33 +0100

    Nog bugging letsencrypt with requests for certificates we already have anymore. Port mapping was in reverse order

--- src/CertBot/Helper/Docker.php
+++ src/CertBot/Helper/Docker.php
@@ -31,7 +31,7 @@ class Docker
             '-v',
             "/var/lib/letsencrypt:/var/lib/letsencrypt",
             '-p',
-            '80:8080',
+            '8080:80',
             'certbot/certbot',
             'certonly',
             '--standalone',
