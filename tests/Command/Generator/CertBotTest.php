e85b9e2d12fbae4acf655000fa20cade5614b503
Anton Boutkam <anton@nui-boutkam.nl>
2020-12-14 17:31:32 +0100

    Fixed failing unit test

--- tests/Command/Generator/CertBotTest.php
+++ tests/Command/Generator/CertBotTest.php
@@ -6,24 +6,45 @@ use Hurah\CertBot\Helper\ApplicationLoader;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
+use Hurah\Types\Exception\InvalidArgumentException;
 
 class CertBotTest extends TestCase
 {
 
-    function testExecute()
+    function testExecuteNoDomain()
     {
+        $this->expectException(InvalidArgumentException::class);
+
         $oApplicationLoader = new ApplicationLoader('Testing CertBot command');
         $oApplication = $oApplicationLoader->get();
         $oCertBotCommand = $oApplication->find('certificate:generate');
 
         $oCommandTester = new CommandTester($oCertBotCommand);
-
-        $this->assertEquals(
-            $oCommandTester->execute([]),
-            Command::FAILURE,
-            "Command should fail when no arguments given");
-
+        $oCommandTester->execute(['--email' => 'fake@novum.nu']);
     }
 
+    function testExecuteNoEmail()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $oApplicationLoader = new ApplicationLoader('Testing CertBot command');
+        $oApplication = $oApplicationLoader->get();
+        $oCertBotCommand = $oApplication->find('certificate:generate');
+
+        $oCommandTester = new CommandTester($oCertBotCommand);
+        $oCommandTester->execute(['--domain' => ['fake.test.com']]);
+    }
+
+    function testExecuteInvalidEmail()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $oApplicationLoader = new ApplicationLoader('Testing CertBot command');
+        $oApplication = $oApplicationLoader->get();
+        $oCertBotCommand = $oApplication->find('certificate:generate');
+
+        $oCommandTester = new CommandTester($oCertBotCommand);
+        $oCommandTester->execute(['--domain' => ['fake.test.com'], '--email' => 'anton@']);
+    }
 
 }
